79a9e2e200c4e11ca38fab1dbe8d3fbb7fa04dd2
Dinesh I <dineshiyyandhurai@gmail.com>
2021-07-18 11:48:38 +0530

    update env setup

--- src/services/Constant.js
+++ src/services/Constant.js
@@ -1,3 +1,6 @@
-const API_ENDPOINT = 'http://localhost:5000/api/v1/';
+const API_ENDPOINT =
+	process.env.NODE_ENV === 'development'
+		? 'http://localhost:5000/api/v1/'
+		: 'https://apimart-api.herokuapp.com/api/v1/';
 
 export { API_ENDPOINT };

427b51cd4480fbf1bbfa5c5b93c18e8ad2feb9d1
Dinesh I <dineshiyyandhurai@gmail.com>
2021-10-30 11:37:32 +0530

    added timeout

--- src/services/AxiosConfig.js
+++ src/services/AxiosConfig.js
@@ -6,6 +6,9 @@ import { API_ENDPOINT } from './Constant';
 axios.defaults.baseURL = API_ENDPOINT;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
+axios.defaults.timeout = 60 * 1000  // ( 60 * seconds ) 1 mins
+axios.defaults.timeoutErrorMessage = "Response timeout takes longer than 1 minutes"
+
 axios.interceptors.request.use(function (config) {
   const userToken = getUserToken();
   if(userToken) {
